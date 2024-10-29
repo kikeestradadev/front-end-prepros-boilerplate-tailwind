69b686f53a5fe94f2f3a67125387b2e2c2530ed8
kikeestrada <kikeestradadev@gmail.com>
2024-10-29 10:55:36 -0600

    first load

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 module.exports = {
 	content: [
 		'**/*.html',
@@ -7,5 +8,22 @@ module.exports = {
 		'!wp-includes',
 		'!node_modules',
 		'!Prepros Export'
-	]
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ['"Inter"', 'sans-serif'],
+			},
+		},
+		screens: {
+			
+			sm: 	'480px',
+			m: 		'640px',
+			l: 		'960px',
+			lg: 	'1280px',
+			xl: 	'1600px',
+			xxl: 	'1800px',
+			xxxl: 	'1920px',
+		},
+	},
 };
